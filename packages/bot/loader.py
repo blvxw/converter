47945edf31dc8cd157f65fe2d9576327400fc267
Bohom Dan <bohdan.lyshuk@gmail.com>
2023-09-12 20:25:58 +0300

    add proxy:

--- packages/bot/loader.py
+++ packages/bot/loader.py
@@ -2,6 +2,16 @@ from aiogram import Bot, Dispatcher, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from packages.utils.env import get_env_variable
 
-bot = Bot(token=get_env_variable("TOKEN"), parse_mode=types.ParseMode.HTML)
+# Задайте дані проксі
+proxy_host = "45.142.252.131"
+proxy_port = 3000
+proxy_username = "QKP1qI"
+proxy_password = "aRCB7dfexC"
+
+# Створіть об'єкт проксі
+proxy = f"http://{proxy_username}:{proxy_password}@{proxy_host}:{proxy_port}/"
+
+# Створіть об'єкт бота з проксі
+bot = Bot(token=get_env_variable("TOKEN"), parse_mode=types.ParseMode.HTML, proxy=proxy)
 storage = MemoryStorage()
 dp = Dispatcher(bot, storage=storage)
